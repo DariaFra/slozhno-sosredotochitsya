41a208e291732d3f759d99bb1b8fe44eb6d09a6f
Daria <frank.darya@yandex.ru>
2024-05-24 11:32:35 +0200

    first try

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/DariaFra/slozhno-sosredotochitsya.git
---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    rules: {
+        'rule-empty-line-before': 'always',
+        'at-rule-empty-line-before': 'always',
+        'no-duplicate-selectors': true
+      }
+  }
+}
